f7eae0d60e89cb0e329eac5ff1674424449d0436
janderkkotlarski <j.d.kotlarski@gmx.com>
2016-03-11 23:53:13 +0100

    new

--- Hydraoids_V0-1.cpp
+++ Hydraoids_V0-1.cpp
@@ -6,55 +6,175 @@
 
 #include <SFML/Graphics.hpp>
 
+void loading(sf::Texture& texture, const std::string& image_name)
+{
+	
+	assert(image_name != "");
+	
+	if (!texture.loadFromFile(image_name))
+	{	
+		std::cerr << image_name << " not found!\n";		
+	}
+
+}
+
+float originate(sf::Sprite& sprite)
+{
+	
+	const float mult{0.27f};
+	assert(mult > 0.0f);
+		
+	const sf::FloatRect image_bounds{sprite.getLocalBounds()};		
+	sprite.setOrigin(0.5f*image_bounds.width, 0.5f*image_bounds.height);
+	return mult*(image_bounds.width + image_bounds.height);
+	
+}
+
+void idle_wait(const float time_quant)
+{
+	
+	sf::Clock clock;
+	
+	while (clock.getElapsedTime().asSeconds() < time_quant)
+	{
+				
+	}
+	
+}
+
+float abs_vector(const sf::Vector2f& vektor)
+{
+	return std::sqrt(vektor.x*vektor.x + vektor.y*vektor.y);
+}
+
+void unbounding(sf::Vector2f& position, const sf::Vector2f& window_xy)
+{
+	
+	while (position.x > window_xy.x)
+	{
+		position.x -= window_xy.x;			
+	}
+	
+	while (position.x < 0)
+	{
+		position.x += window_xy.x;			
+	}
+	
+	while (position.y > window_xy.y)
+	{
+		position.y -= window_xy.y;			
+	}
+	
+	while (position.y < 0)
+	{
+		position.y += window_xy.y;			
+	}
+	
+}
+
+sf::Vector2f direction_accel(sf::RenderWindow &window, const sf::Vector2f& position)
+{
+	
+	const float mult{1.0f};
+	assert(mult > 0.0f);
+	
+	const sf::Vector2f direction{static_cast<sf::Vector2f>(sf::Mouse::getPosition(window)) - position};
+	
+	sf::Vector2f accel{0.0f, 0.0f};
+	
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+	{
+		accel = mult*direction/abs_vector(direction);
+	}
+	
+	return accel;
+	
+}
+
 class player
 {
 	 
 	const std::string m_image_name{"Ball.png"};
-	
-	float m_radius{0.0f};
-	
+		
 	sf::Vector2f m_position{0.0f, 0.0f};
-	
 	sf::Vector2f m_speed{0.0f, 0.0f};
-	
 	sf::Vector2f m_accel{0.0f, 0.0f};
 	
 	const sf::Color m_light_blue{127, 127, 255};
 	
 	sf::Texture m_texture;
-	
 	sf::Sprite m_sprite;
+	
+	float m_radius{0.0f};
 	
 	void load_image()
 	{
-	
-		assert(m_image_name != "");
-		
-		if (!m_texture.loadFromFile(m_image_name))
-		{
-		
-			std::cerr << m_image_name << " not found!\n";
-			
-		}
-	
+		loading(m_texture, m_image_name);
 	}
 	
 	void texture_in_sprite()
 	{
-		
 		m_sprite.setTexture(m_texture);
-		
 	}
 	
 	void set_origin()
 	{
+		m_radius = originate(m_sprite);
+	}
+	
+	void set_position()
+	{
+		m_sprite.setPosition(m_position);
+	}
+	
+	void set_color()
+	{
+		m_sprite.setColor(m_light_blue);
+	}
+	
+	public:
+	
+	void set_accel(sf::RenderWindow &window)
+	{
+		m_accel = direction_accel(window, m_position);
+	}
+	
+	void change_speed(const float time_delta)
+	{
+		m_speed += time_delta*m_accel;
+	}
+	
+	void move_player(const float time_delta)
+	{
+		m_position += time_delta*m_speed;
+		set_position();
+	}
+	
+	void edge_overlap(const sf::Vector2f& window_xy)
+	{
+		unbounding(m_position, window_xy);		
+		set_position();
+	}
+	
+	void show_object(sf::RenderWindow& window)
+	{
+		window.draw(m_sprite);
+	}
+	
+	player(const sf::Vector2f& position)
+		: m_position(position), m_texture(), m_sprite()
+	{
 		
-		const float mult{0.27f};
-		assert(mult > 0.0f);
+		load_image();
+		texture_in_sprite();
+		set_origin();
+		set_position();
+		set_color();
 		
-		const sf::FloatRect m_image_bounds{m_sprite.getLocalBounds()};		
-		m_sprite.setOrigin(0.5f*m_image_bounds.width, 0.5f*m_image_bounds.height);
-		m_radius = mult*(m_image_bounds.width + m_image_bounds.height);
+	}
+	
+	~player()
+	{
 		
 	}
 	
@@ -66,10 +186,63 @@ int main()
 	const std::string program_name{"Hydraoids V0.1"};
 	assert(program_name != "");
 	
+	const float window_x{768.0f};
+	const float window_y{768.0f};
 	
+	const sf::Vector2f window_xy{window_x, window_y};
 	
+	const float time_quant{0.001f};
+	const float time_delta{25.0f*time_quant};
 	
+	const sf::Color black{sf::Color(0, 0, 0)};
 	
+	player player_circle{0.5f*window_xy};
+	
+	sf::RenderWindow window{sf::VideoMode(window_x, window_y), program_name, sf::Style::Default};
+	
+	while (window.isOpen())
+    {
+		
+		sf::Clock clock;
+		
+		sf::Event event;
+		
+		window.clear(black);
+		
+		player_circle.show_object(window);
+		
+		window.display();
+		
+		while (clock.getElapsedTime().asSeconds() < time_delta)
+		{
+			
+			player_circle.set_accel(window);
+			player_circle.change_speed(time_quant);
+			player_circle.move_player(time_quant);
+			player_circle.edge_overlap(window_xy);
+			
+			idle_wait(time_quant);
+			
+		}
+		
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window.close();                    
+			return 1;
+		}
+		
+		while (window.pollEvent(event))
+		{
+			
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();                
+				return 2;
+			}
+		
+		}
+		
+	}	
 	
 	return 0;
 
---
+++ Hydraoids_V0-2.cpp
@@ -0,0 +1,437 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <cassert>
+
+#include <SFML/Graphics.hpp>
+
+void loading(sf::Texture& texture, const std::string& image_name)
+{
+	
+	assert(image_name != "");
+	
+	if (!texture.loadFromFile(image_name))
+	{	
+		std::cerr << image_name << " not found!\n";		
+	}
+
+}
+
+float originate(sf::Sprite& sprite)
+{
+	
+	const float mult{0.27f};
+	assert(mult > 0.0f);
+		
+	const sf::FloatRect image_bounds{sprite.getLocalBounds()};		
+	sprite.setOrigin(0.5f*image_bounds.width, 0.5f*image_bounds.height);
+	return mult*(image_bounds.width + image_bounds.height);
+	
+}
+
+void idle_wait(const float time_quant)
+{
+	
+	sf::Clock clock;
+	
+	while (clock.getElapsedTime().asSeconds() < time_quant)
+	{
+				
+	}
+	
+}
+
+float abs_vector(const sf::Vector2f& vektor)
+{
+	return std::sqrt(vektor.x*vektor.x + vektor.y*vektor.y);
+}
+
+void unbounding(sf::Vector2f& position, const sf::Vector2f& window_xy)
+{
+	
+	while (position.x > window_xy.x)
+	{
+		position.x -= window_xy.x;			
+	}
+	
+	while (position.x < 0)
+	{
+		position.x += window_xy.x;			
+	}
+	
+	while (position.y > window_xy.y)
+	{
+		position.y -= window_xy.y;			
+	}
+	
+	while (position.y < 0)
+	{
+		position.y += window_xy.y;			
+	}
+	
+}
+
+sf::Vector2f unity_vector(const sf::Vector2f& vektor)
+{
+	
+	if ((vektor.x != 0.0f) || (vektor.y != 0.0f))
+	{
+		return vektor/abs_vector(vektor);
+	}
+	
+	return vektor;
+	
+}
+
+sf::Vector2f mouse_vector(sf::RenderWindow &window, const sf::Vector2f& position)
+{
+	return static_cast<sf::Vector2f>(sf::Mouse::getPosition(window)) - position;
+}
+
+sf::Vector2f direction_accel(sf::RenderWindow &window, const sf::Vector2f& position)
+{
+	
+	const float mult{100.0f};
+	assert(mult > 0.0f);
+	
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+	{
+		return mult*unity_vector(mouse_vector(window, position));
+	}
+	
+	return sf::Vector2f (0.0f, 0.0f);
+	
+}
+
+class player
+{
+	 
+	const std::string m_image_name{"Ball.png"};
+		
+	sf::Vector2f m_position{0.0f, 0.0f};
+	sf::Vector2f m_speed{0.0f, 0.0f};
+	sf::Vector2f m_accel{0.0f, 0.0f};
+	
+	const sf::Color m_light_blue{127, 127, 255};
+	
+	sf::Texture m_texture;
+	sf::Sprite m_sprite;
+	
+	float m_radius{0.0f};
+	
+	void load_image()
+	{
+		loading(m_texture, m_image_name);
+	}
+	
+	void texture_in_sprite()
+	{
+		m_sprite.setTexture(m_texture);
+	}
+	
+	void set_origin()
+	{
+		m_radius = originate(m_sprite);
+	}
+	
+	void set_position()
+	{
+		m_sprite.setPosition(m_position);
+	}
+	
+	void set_color()
+	{
+		m_sprite.setColor(m_light_blue);
+	}
+	
+	public:
+	
+	void set_accel(sf::RenderWindow &window)
+	{
+		m_accel = direction_accel(window, m_position);
+	}
+	
+	void change_speed(const float time_delta)
+	{
+		m_speed += time_delta*m_accel;
+	}
+	
+	void move_player(const float time_delta)
+	{
+		m_position += time_delta*m_speed;
+		set_position();
+	}
+	
+	void edge_overlap(const sf::Vector2f& window_xy)
+	{
+		unbounding(m_position, window_xy);		
+		set_position();
+	}
+	
+	void show_object(sf::RenderWindow& window)
+	{
+		window.draw(m_sprite);
+	}
+	
+	float show_radius()
+	{
+		return m_radius;
+	}
+	
+	sf::Vector2f show_position()
+	{
+		return m_position;
+	}
+	
+	player(const sf::Vector2f& position)
+		: m_position(position), m_texture(), m_sprite()
+	{
+		
+		load_image();
+		texture_in_sprite();
+		set_origin();
+		set_position();
+		set_color();
+		
+	}
+	
+	~player()
+	{
+		
+	}
+	
+};
+
+class hydra
+{
+	
+	const std::string m_image_name{"Ball.png"};
+		
+	sf::Vector2f m_position{0.0f, 0.0f};
+	sf::Vector2f m_speed{0.0f, 0.0f};
+	
+	const sf::Color m_light_orange{255, 195, 127};
+	const sf::Color m_light_red{255, 127, 127};
+	
+	sf::Color m_color{m_light_red};
+	
+	sf::Texture m_texture;
+	sf::Sprite m_sprite;
+	
+	float m_radius{0.0f};
+	
+	void load_image()
+	{
+		loading(m_texture, m_image_name);
+	}
+	
+	void texture_in_sprite()
+	{
+		m_sprite.setTexture(m_texture);
+	}
+	
+	void set_origin()
+	{
+		m_radius = originate(m_sprite);
+	}
+	
+	void set_position()
+	{
+		m_sprite.setPosition(m_position);
+	}
+	
+	void set_color()
+	{
+		m_sprite.setColor(m_color);
+	}
+	
+	void intro_spedition(sf::RenderWindow& window, player& player_object)
+	{
+		
+		const float mult_posit{2.2f};
+		assert(mult_posit > 2.0f);
+		
+		const float mult_speed{10.0f};
+		assert(mult_speed > 0.0f);
+		
+		const sf::Vector2f unit_vect{unity_vector(mouse_vector(window, player_object.show_position()))};
+		
+		m_position = mult_posit*player_object.show_radius()*unit_vect + player_object.show_position();
+		
+		m_speed = mult_speed*unit_vect;
+		
+	}
+	
+	public:
+		
+	void move_hydra(const float time_delta)
+	{
+		m_position += time_delta*m_speed;
+		set_position();
+	}
+	
+	void edge_overlap(const sf::Vector2f& window_xy)
+	{
+		unbounding(m_position, window_xy);		
+		set_position();
+	}
+	
+	void show_object(sf::RenderWindow& window)
+	{
+		window.draw(m_sprite);
+	}
+	
+	void confirm_sprite()
+	{
+		load_image();
+		texture_in_sprite();		
+	}
+	
+	hydra(sf::RenderWindow& window, player& player_object)
+		: m_texture(), m_sprite()
+	{
+		
+		intro_spedition(window, player_object);
+		confirm_sprite();
+		set_origin();
+		set_position();
+		set_color();
+		
+	}
+	
+	~hydra()
+	{
+		
+	}
+	
+};
+
+void add_hydra(std::vector <hydra>& hydra_objects, sf::RenderWindow& window, player& player_object)
+{
+	
+	hydra_objects.push_back(hydra (window, player_object));
+	
+}
+
+int main()
+{
+	
+	const std::string program_name{"Hydraoids V0.2"};
+	assert(program_name != "");
+	
+	const float window_x{768.0f};
+	const float window_y{768.0f};
+	
+	const sf::Vector2f window_xy{window_x, window_y};
+	
+	const float time_quant{0.001f};
+	const float time_delta{25.0f*time_quant};
+	
+	const sf::Color black{sf::Color(0, 0, 0)};
+	
+	player player_object{0.5f*window_xy};
+	
+	std::vector <hydra> hydra_objects;
+	int hydra_count{0};
+	
+	bool hydra_launched{false};
+	
+	sf::RenderWindow window{sf::VideoMode(window_x, window_y), program_name, sf::Style::Default};
+	
+	while (window.isOpen())
+    {
+		
+		sf::Clock clock;
+		
+		sf::Event event;
+		
+		window.clear(black);
+		
+		if (hydra_count > 0)
+		{
+			int count{0};
+			
+			while(count < hydra_count)
+			{
+				hydra_objects[count].show_object(window);
+				++count;
+			}
+		}
+		
+		player_object.show_object(window);
+		
+		window.display();
+		
+		while (clock.getElapsedTime().asSeconds() < time_delta)
+		{
+			
+			sf::Clock timer;
+			
+			player_object.set_accel(window);
+			player_object.change_speed(time_quant);
+			player_object.move_player(time_quant);
+			player_object.edge_overlap(window_xy);
+			
+			// std::cout << timer.getElapsedTime().asSeconds() << "!\n";
+			
+			while (timer.getElapsedTime().asSeconds() < time_quant)
+			{
+				// std::cout << timer.getElapsedTime().asSeconds() << "\n";
+			}
+			
+			// std::cout << timer.getElapsedTime().asSeconds() << "?\n";
+			
+			// idle_wait(time_quant);
+			
+		}
+		
+		// window.close();
+		// return 3;
+		
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left) &&
+			!hydra_launched)
+		{
+			hydra_launched = true;
+		}
+		
+		if (!sf::Mouse::isButtonPressed(sf::Mouse::Left) &&
+			hydra_launched)
+		{
+			add_hydra(hydra_objects, window, player_object);
+			++hydra_count;
+			
+			int count{0};
+			while (count < hydra_count)
+			{
+				
+				hydra_objects[count].confirm_sprite();
+				++count;
+				
+			}
+			
+			hydra_launched = false;
+		}
+		
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window.close();                    
+			return 1;
+		}
+		
+		while (window.pollEvent(event))
+		{
+			
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();                
+				return 2;
+			}
+		
+		}
+		
+	}	
+	
+	return 0;
+
+}
